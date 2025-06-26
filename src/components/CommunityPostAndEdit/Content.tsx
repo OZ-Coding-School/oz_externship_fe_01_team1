70275f1a41f634f1dd4fd8889d5821202a0b6a29
yongarframe <yongar2002@gmail.com>
2025-06-26 15:58:12 +0900

    fix: 린트 오류 파일 삭제

--- src/components/CommunityPostAndEdit/Content.tsx
+++ src/components/CommunityPostAndEdit/Content.tsx
@@ -1,28 +1,22 @@
 import MDEditor from '@uiw/react-md-editor';
 
-interface Props {
-    markdown: string;
-    setMarkdown: (val: string) => void;
-}
+export default function Content() {
+    const [markdown, setMarkdown] = useState('');
 
-export default function Content({markdown, setMarkdown}: Props) {
     return (
-        <section>
-            <div className="max-w-5xl mx-auto bg-white   rounded-xl border border-gray-200 mt-10 gap-10">
-                <div className="bg-white"  data-color-mode="light">
+        <>
+            <div className="max-w-5xl mx-auto bg-white   rounded-xl border border-gray-200 mt-10">
+                <div className="bg-white rotate-md" data-color-mode="light">
                     <MDEditor
                         value={markdown}
-                        onChange={(val: string | undefined) => setMarkdown(val ?? '')}
+                        onChange={(val = '') => setMarkdown(val)}
                         height={400}
                         preview="live"
                         style={{backgroundColor: 'white', color: 'black'}}
-                        previewOptions={{
-                            className: 'bg-white text-black',
-                            style: {backgroundColor: 'white', color: 'black'}
-                        }}
+                        previewOptions={{className: 'bg-white', style: {color: 'black'}}}
                     />
                 </div>
             </div>
-        </section>
-    );
+        </>
+    )
 }
