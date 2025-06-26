4986b6513d1dc2059d0524220a9cb15527b84c67
Lee-sung-il <wj132612@naver.com>
2025-06-26 17:19:07 +0900

    커밋 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
-import CommunityDetail from './pages/ComunityDetail'
+import CommunityDetail from './pages/CommunityDetail'
 import Layout from './pages/Layout'
 import CommunityList from './pages/CommunityList'
 import Login from './pages/Login'
 import Join from './pages/Join'
+<<<<<<< HEAD
 import CommunityPost from './pages/CommunityPost'
+=======
+import { CommunityPost } from './pages/CommunityPost'
+>>>>>>> e929f05 (오타 수정 및 댓글 정렬 기능 커스텀훅으로 적용)
 import CommunityEdits from './pages/CommunityEdits'
 import NotFound from './pages/NotFound'
 
@@ -29,7 +33,10 @@ export default function App() {
         />
         <Route path="/Join" element={<Join />} />
         <Route path="/Login" element={<Login />} />
+<<<<<<< HEAD
         <Route path="*" element={<NotFound />} />
+=======
+>>>>>>> e929f05 (오타 수정 및 댓글 정렬 기능 커스텀훅으로 적용)
       </Route>
     </Routes>
   )
--- src/components/CommunityPostAndEdit/Content.tsx
+++ src/components/CommunityPostAndEdit/Content.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 export default function Content() {
