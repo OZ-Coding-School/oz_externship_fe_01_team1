03886d9cadd070d6671e66d6d6bec2d852677880
Lee-sung-il <wj132612@naver.com>
2025-06-26 17:27:46 +0900

    프로필 png 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import Layout from './pages/Layout'
 import CommunityList from './pages/CommunityList'
 import Login from './pages/Login'
 import Join from './pages/Join'
-<<<<<<< HEAD
 import CommunityPost from './pages/CommunityPost'
-=======
-import { CommunityPost } from './pages/CommunityPost'
->>>>>>> e929f05 (오타 수정 및 댓글 정렬 기능 커스텀훅으로 적용)
 import CommunityEdits from './pages/CommunityEdits'
 import NotFound from './pages/NotFound'
 
@@ -33,10 +29,7 @@ export default function App() {
         />
         <Route path="/Join" element={<Join />} />
         <Route path="/Login" element={<Login />} />
-<<<<<<< HEAD
         <Route path="*" element={<NotFound />} />
-=======
->>>>>>> e929f05 (오타 수정 및 댓글 정렬 기능 커스텀훅으로 적용)
       </Route>
     </Routes>
   )
