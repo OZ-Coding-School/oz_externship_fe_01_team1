ffcad0d131066922155db8a2725b426f18113620
yongarframe <yongar2002@gmail.com>
2025-06-28 20:54:19 +0900

    ✨ feat: center-bg 배경 흐림 기능 추가 및 Button children 방식 적용
    커뮤니티 상세 페이지용 modal 작성하였고, 좀더 범용적으로 사용하기 위해서 수정
    
    관련 이슈: #32

--- src/components/commnunityDetail/Comment.tsx
+++ src/components/commnunityDetail/Comment.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { formatDate } from '../../lib'
 import type { commentData } from '../../types'
-import ModalCommentDel from './ModalCommentDel'
+import CommonModal from '@components/common/Modal'
+import { Button } from '@components/common'
 
 export default function Comment({
   commentData: { name, date, content, imgUrl },
@@ -25,11 +26,32 @@ export default function Comment({
           >
             삭제
           </div>
+          {
+            <CommonModal
+              isOpen={isModal}
+              onClose={() => setIsModal(false)}
+              position="center-bg"
+              title="댓글을 삭제하시겠습니까?"
+            >
+              <Button
+                fullWidth={false}
+                className="flex justify-center items-center px-[24px] py-[18px] bg-[#efe6fc] text-[16px] text-[#4e01b3] font-[600] rounded-[100px] h-[43px] w-[76px]"
+                onClick={() => setIsModal(false)}
+              >
+                취소
+              </Button>
+              <Button
+                fullWidth={false}
+                className="flex justify-center items-center px-[24px] py-[18px] bg-[#6201e0] text-[16px] text-[#fafafa] font-[600] rounded-[100px] h-[43px] w-[76px]"
+                onClick={() => setIsModal(false)}
+              >
+                확인
+              </Button>
+            </CommonModal>
+          }
         </div>
         <div className="text-[#000000] text-[16px] font-[400]">{content}</div>
       </div>
-      {isModal && <ModalCommentDel setIsModal={setIsModal} />}
     </div>
   )
 }
-
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,63 +1,75 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 interface CommonModalProps {
+  title: string
   isOpen: boolean
   onClose: () => void
   children: React.ReactNode
+  position?: 'center' | 'inline' | 'center-bg'
 }
 
 export default function CommonModal({
   isOpen,
   onClose,
   children,
+  position = 'center',
+  title,
 }: CommonModalProps) {
+  const modalRef = useRef<HTMLDivElement>(null)
+
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        onClose()
+      if (e.key === 'Escape') onClose()
+    }
+    // 외부 클릭 시 창닫기
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        modalRef.current && // ref current 가 존재하고
+        e.target instanceof Node && // 클릭 이 node 이고
+        !modalRef.current.contains(e.target) // null 이 아닐때
+      ) {
+        onClose() // 창닫힘
       }
     }
 
     if (isOpen) {
       window.addEventListener('keydown', handleKeyDown)
+      if (position === 'inline') {
+        document.addEventListener('mousedown', handleClickOutside)
+      }
     }
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown)
+      if (position === 'inline') {
+        document.removeEventListener('mousedown', handleClickOutside)
+      }
     }
-  }, [isOpen, onClose])
+  }, [isOpen, onClose, position])
 
   if (!isOpen) return null
 
+  const modalContent = (
+    <div
+      ref={modalRef}
+      onClick={(e) => e.stopPropagation()}
+      className={`${position === 'inline' ? 'absolute top-[-70px] left-[300px]' : ''} w-[428px] h-[162px] bg-white shadow-[0_4px_16px_rgba(0,0,0,0.25)] rounded-[24px] p-[24px] flex flex-col justify-between`}
+    >
+      <div className="text-[14px] mb-[12px]">{title}</div>
+      <div className="flex justify-end gap-[12px]">{children}</div>
+    </div>
+  )
+
+  if (position === 'inline') {
+    return modalContent
+  }
+
   return (
     <div
-      className="absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-opacity-50"
+      className={`fixed inset-0 z-50 flex items-center justify-center ${position === 'center-bg' ? 'bg-[rgba(0,0,0,0.2)]' : ''}`}
       onClick={onClose}
     >
-      <div
-        role="dialog"
-        aria-modal="true"
-        onClick={(e) => e.stopPropagation()}
-        className=" w-[428px] h-[162px] bg-white shadow-[0_4px_16px_rgba(0,0,0,0.25)] rounded-[24px] p-[24px] flex flex-col justify-between"
-      >
-        <div>{children}</div>{' '}
-        <div className="flex justify-end gap-[12px]">
-          <button
-            onClick={onClose}
-            className="flex justify-center items-center px-[24px] py-[18px] bg-[#efe6fc] text-[16px] text-[#4e01b3] font-[600] rounded-[100px] h-[43px] w-[76px]"
-          >
-            취소
-          </button>
-          <button
-            onClick={onClose}
-            className="flex justify-center items-center px-[24px] py-[18px] bg-[#6201e0] text-[16px] text-[#fafafa] font-[600] rounded-[100px] h-[43px] w-[76px]"
-          >
-            확인
-          </button>
-        </div>
-      </div>
+      {modalContent}
     </div>
   )
 }
-
-//const [open, setOpen] = useState(false);<CommonModal isOpen={open} onClose={() => setOpen(false)} title="모달 제목"><p>모달 내용</p></CommonModal>
