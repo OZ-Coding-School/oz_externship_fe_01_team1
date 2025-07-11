29b09e3562d1d8abb06658e58326d89be5ab42d2
yongarframe <yongar2002@gmail.com>
2025-07-11 16:43:04 +0900

    🐛 fix : access 토큰 만료 시 refresh 토큰 자동 요청

--- src/api/mainApi.ts
+++ src/api/mainApi.ts
@@ -1,5 +1,5 @@
 import type { userData } from '@customType/userData'
-import axios from 'axios'
+import axios, { type AxiosRequestConfig } from 'axios'
 
 // 인스턴스 생성
 const api = axios.create({
@@ -7,6 +7,7 @@ const api = axios.create({
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json', // 공통 헤더
+    Authorization: '',
   },
   withCredentials: true,
 })
@@ -29,29 +30,52 @@ api.interceptors.request.use(
   }
 )
 
-// export const testApi = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   timeout: 5000,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// })
+const refreshAccessToken = async () => {
+  const userInfo = localStorage.getItem('userInfo')
+  if (!userInfo) throw new Error('No userInfo found in localStorage')
 
-// testApi.interceptors.request.use(
-//   (config) => {
-//     const userInfo = localStorage.getItem('userInfo')
+  const parsed: { state: { userInfo: userData } } = JSON.parse(userInfo)
+  const refreshToken = parsed.state.userInfo.refresh
+  if (!refreshToken) throw new Error('No refresh token found')
 
-//     if (userInfo) {
-//       const parsed: userData = JSON.parse(userInfo)
-//       const token = parsed.access
-//       config.headers.Authorization = `Bearer ${token}`
-//     }
-//     return config
-//   },
-//   (error) => {
-//     console.error('Request error:', error)
-//     return Promise.reject(error)
-//   }
-// )
+  const res = await axios.post('v1/auth/token/refresh/', {
+    refresh: refreshToken,
+  })
+
+  const newAccessToken: string = res.data.accessToken
+  parsed.state.userInfo.access = newAccessToken
+  localStorage.setItem('userInfo', JSON.stringify(parsed))
+
+  return newAccessToken
+}
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config as AxiosRequestConfig & {
+      _retry?: boolean
+    }
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true
+      try {
+        const newAccessToken = await refreshAccessToken()
+        originalRequest.headers = {
+          ...originalRequest.headers,
+          Authorization: `Bearer ${newAccessToken}`,
+        }
+        return api(originalRequest)
+      } catch (refreshError) {
+        localStorage.removeItem('userInfo')
+        window.location.href = '/login'
+        return Promise.reject(refreshError)
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default api
