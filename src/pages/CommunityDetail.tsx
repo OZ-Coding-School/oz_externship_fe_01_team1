ccd56d11adcd06e90899e5a392a36d414dbb73bc
yongarframe <yongar2002@gmail.com>
2025-07-14 18:44:24 +0900

    🐛 fix : 백엔드 api 수정 및 persist 적용하면서 access token 객체 구조 변화로 mainapi 로직 수정

--- src/pages/CommunityDetail.tsx
+++ src/pages/CommunityDetail.tsx
@@ -14,7 +14,7 @@ export default function CommunityDetail() {
 
   useEffect(() => {
     const fetchDetail = async () => {
-      const res = await api.get<DetailData>(`/v1/community/admin/posts/${id}`)
+      const res = await api.get<DetailData>(`/v1/community/posts/${id}`)
       setDetailData(res.data)
     }
     fetchDetail()
